ba7ad0ad4474e6458a8346dd805ba550bb381b5a
R4FFF <69420740+R4FFF@users.noreply.github.com>
2021-11-30 18:10:56 +0000

    apartment page test

--- components/carousel/Carousel.js
+++ components/carousel/Carousel.js
@@ -64,7 +64,7 @@ const Carousel = ({ data }) => {
                     scrollbar={{ draggable: true }}
                     >    
                         {data && data.map(item => (
-                            <SwiperSlide key={item.id} ><div className={classes.imgWrapper} ><Image layout="fill" objectFit="cover" src={item.url} alt="Apartment pictures" priority /></div></SwiperSlide>
+                            <SwiperSlide key={item.id}><div className={classes.imgWrapper} ><Image layout="fill" objectFit="cover" src={item.url} alt="Apartment pictures" /></div></SwiperSlide>
                         ))}
               </Swiper>
           </SRLWrapper>  
